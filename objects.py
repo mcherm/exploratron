a03d8cb77aaa2d5183208dd6d59e6f0dfd359511
Michael Chermside <mcherm@mcherm.com>
2017-05-20 21:45:25 -0400

    A few more types of objects were created.

--- objects.py
+++ objects.py
@@ -5,3 +5,14 @@ class BrickWall(Wall):
     def __init__(self):
         Wall.__init__(self, 7)
 
+class Dirt(Thing):
+    def __init__(self):
+        Thing.__init__(self, 0)
+
+class StairsDown(Thing):
+    def __init__(self):
+        Thing.__init__(self, 5)
+
+class Chest(Thing):
+    def __init__(self):
+        Thing.__init__(self, 6)
