504001c80c30328616ebbd43c1623f5b47c40ba9
Michael Chermside <mcherm@mcherm.com>
2017-07-04 01:21:37 -0400

    Minor advance: accept KeyPressedMessage on demo server.

--- networktestclient.py
+++ networktestclient.py
@@ -9,19 +9,23 @@ def messageClient():
     print(f'Test Client')
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(1)
-    addr = ("127.0.0.1", 12000)
+    serverAddr = ("127.0.0.1", 12000)
 
     start = time.time()
     byteStr = JoinServerMessage().toBytes()
     if len(byteStr) > UDP_MAX_SIZE:
         raise Exception("Message too long for our UDP buffers.")
-    clientSocket.sendto(byteStr, addr)
+    clientSocket.sendto(byteStr, serverAddr)
     try:
-        data, server = clientSocket.recvfrom(1024)
+        data, server = clientSocket.recvfrom(UDP_MAX_SIZE)
         end = time.time()
         elapsed = end - start
         print(f"{data} {elapsed}")
     except timeout:
         print("REQUEST TIMED OUT")
-    
+    byteStr = KeyPressedMessage(115).toBytes()
+    if len(byteStr) > UDP_MAX_SIZE:
+        raise Exception("Message too long for our UDP buffers.")
+    clientSocket.sendto(byteStr, serverAddr)
+
 messageClient()
