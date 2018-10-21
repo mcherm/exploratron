40812b557d6f798c7e3e28ee763f2583e9a82a6f
Michael Chermside <mcherm@mcherm.com>
2018-10-21 11:13:02 -0400

    Created a ?maxHealth?.

--- mobile.py
+++ mobile.py
@@ -6,18 +6,20 @@ class Stats:
         """Create a new Stats, with all stats set to 0."""
         self.strength = 0
         self.speed = 0
+        self.maxHealth = 0
         self.health = 0
 
 
 
 class Mobile(Thing):
-    def __init__(self, region, tileName, health, inventory=()):
+    def __init__(self, region, tileName, maxHealth, inventory=()):
         super().__init__(region, tileName)
         self.whenItCanAct = 0
         self.isDead = False
         self.inventory = [x for x in inventory]
         self.stats = Stats()
-        self.stats.health = health
+        self.stats.maxHealth = maxHealth
+        self.stats.health = maxHealth
         self.stats.speed = 5
     def canEnter(self, mobile):
         return False
@@ -152,7 +154,7 @@ class Mobile(Thing):
         self.whenItCanAct = currentTime + self.timeToWait()
     def takeDamage(self, amount):
         self.stats.health = self.stats.health-amount
-        print(self.stats.health)
+        print(self.stats.health) # FIXME: This can be removed
         if self.stats.health < 1:
             self.isDead = True
             print (self.isDead)
