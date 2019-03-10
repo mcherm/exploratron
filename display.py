397eaa55ee78ad8ad6721aa7592bb2f6f03d630e
Michael Chermside <mcherm@mcherm.com>
2019-03-10 11:55:23 -0400

    Console now displays on the screen, with randomly-generated junk messages just to give us something to see.

--- display.py
+++ display.py
@@ -5,30 +5,35 @@ from images import TILE_SIZE
 from inventory_view import LocalInventoryView, RemoteInventoryView
 from clientdata import GridData
 from exploranetworking import RequestInventoryMessage
-
-
-
-class PygameGridDisplay:
-    def __init__(self, screen):
-        self.screen = screen
-
-    def show(self, gridData, uiState, imageLibrary):
-        assert isinstance(gridData, GridData)
-        self.screen.fill((0, 0, 0))
-        screenWidth, screenHeight = uiState.screenWidthAndHeight
-        for screenY in range(min(screenHeight, gridData.height)):
-            for screenX in range(min(screenWidth, gridData.width)):
-                offsetX, offsetY = uiState.offset
-                cellData = gridData.cellAt(screenX + offsetX, screenY + offsetY)
-                for tileId in cellData.tileIds():
-                    image = imageLibrary.lookupById(tileId)
-                    self.screen.blit(image, (TILE_SIZE * screenX, TILE_SIZE * screenY))
+from console import Console
 
 
 LIGHT_GREY = (120, 120, 120)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 PURPLE = (144, 9, 189)
+STARTING_WIDTH_IN_TILES = 16
+STARTING_HEIGHT_IN_TILES = 10
+CONSOLE_HEIGHT_IN_PIXELS = 8 * 19
+
+
+class PygameGridDisplay:
+    def show(self, screen, position, gridData, uiState, imageLibrary):
+        """Draws the display to the screen. screen is the surface on which to draw and position is
+        a rect within which it should do the drawing. For now, it is an error if the width and
+        height of position are not multiples of TILE_SIZE."""
+        assert isinstance(gridData, GridData)
+        screen.fill(BLACK, position)
+        screenWidth, screenHeight = uiState.screenWidthAndHeight
+        assert position.width == TILE_SIZE * screenWidth
+        assert position.height == TILE_SIZE * screenHeight
+        for screenY in range(min(screenHeight, gridData.height)):
+            for screenX in range(min(screenWidth, gridData.width)):
+                offsetX, offsetY = uiState.offset
+                cellData = gridData.cellAt(screenX + offsetX, screenY + offsetY)
+                for tileId in cellData.tileIds():
+                    image = imageLibrary.lookupById(tileId)
+                    screen.blit(image, (position.left + TILE_SIZE * screenX, position.top + TILE_SIZE * screenY))
 
 
 class PygameOverlayDisplay:
@@ -86,20 +91,30 @@ class PygameDisplay:
     of the grid. It also deals with playing sounds."""
 
     def __init__(self):
-        STARTING_WIDTH_IN_TILES = 16
-        STARTING_HEIGHT_IN_TILES = 11
         self.uiState = UIState(STARTING_WIDTH_IN_TILES, STARTING_HEIGHT_IN_TILES)
         self.screen = pygame.display.set_mode(
-            (STARTING_WIDTH_IN_TILES * TILE_SIZE, STARTING_HEIGHT_IN_TILES * TILE_SIZE))
+            (STARTING_WIDTH_IN_TILES * TILE_SIZE, STARTING_HEIGHT_IN_TILES * TILE_SIZE + CONSOLE_HEIGHT_IN_PIXELS))
         pygame.event.set_allowed(None)
         pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN])
 
-        self.gridDisplay = PygameGridDisplay(self.screen)
+        self.gridPosition = self.screen.get_rect()
+        self.gridPosition.height = STARTING_HEIGHT_IN_TILES * TILE_SIZE
+        self.consolePosition = self.screen.get_rect()
+        self.consolePosition.top = self.gridPosition.bottom
+        self.consolePosition.height = CONSOLE_HEIGHT_IN_PIXELS
+
+        self.gridDisplay = PygameGridDisplay()
         self.overlayDisplay = PygameOverlayDisplay(self.screen)
+        self.console = Console(self.consolePosition.size)
 
     def show(self, gridData, imageLibrary):
-        self.gridDisplay.show(gridData, self.uiState, imageLibrary)
+        self.gridDisplay.show(self.screen, self.gridPosition, gridData, self.uiState, imageLibrary)
         self.overlayDisplay.show(self.uiState, imageLibrary)
+        # FIXME: Begin of generating random text just to test ---------------------------------
+        import datetime
+        self.console.addMessage( f"{(datetime.datetime.now())} - {len(self.console.messages)} messages" )
+        # FIXME: End of generating random text just to test -----------------------------------
+        self.console.show(self.screen, self.consolePosition)
         pygame.display.flip()
 
     def playSounds(self, soundEffectIds, soundLibrary):
